fe689eeb94804754bbd2997a3923050029ee69c5
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-23 02:02:23 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// Masala: K butun berilgan baho natijalarini tuzing. 1-yomon, 2-qoniqarsiz, 3-qoniqarli, 4-yaxshi 5-alo sozlari ekranga chiqsin
+
+let K=4;
+
+switch(K){
+    case 1:
+        console.log("Yomon");
+        break;
+
+    case 2 :
+        console.log("Qoniqarsiz");
+        break;
+
+    case 3 :
+        console.log("Qoniqarli");
+        break;
+
+    case 4:
+        console.log("Yaxshi");
+        break;
+
+    case 5:
+        console.log("A'lo");
+        break;
+}
+
